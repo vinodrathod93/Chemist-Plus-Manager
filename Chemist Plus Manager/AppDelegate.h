7fe58f94c3111b306d887a43f5d9e3df84aec726
vinodrathod93 <vinod.k.rathod93@gmail.com>
2015-07-09 09:39:42 +0530

    Initial Commit

---
+++ Chemist Plus Manager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chemist Plus Manager
+//
+//  Created by adverto on 09/07/15.
+//  Copyright (c) 2015 adverto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chemist Plus ManagerTests/Chemist_Plus_ManagerTests.m
@@ -0,0 +1,40 @@
+//
+//  Chemist_Plus_ManagerTests.m
+//  Chemist Plus ManagerTests
+//
+//  Created by adverto on 09/07/15.
+//  Copyright (c) 2015 adverto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Chemist_Plus_ManagerTests : XCTestCase
+
+@end
+
+@implementation Chemist_Plus_ManagerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
